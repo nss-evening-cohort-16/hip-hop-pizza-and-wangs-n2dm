880bdc030137eb80fddd88f1a60729b200872c7b
Nathan Blackman <nathanpgblackman@gmail.com>
2021-09-15 20:52:26 -0500

    added submit button and fixed most of the form aside from the drop down portion

---
+++ src/scripts/components/forms/createOrderForm.js
@@ -0,0 +1,29 @@
+import clearDom from '../../helpers/clearDom';
+
+const createOrderForm = (userId, obj = {}) => {
+  clearDom();
+  document.querySelector('#form-container').innerHTML = `<div class="mb-3">
+  <label for="orderName" class="form-label">Order Name</label>
+  <input type="text" class="form-control" id="orderName" value="${obj.orderName}" required>
+</div>
+<div class="mb-3">
+  <label for="customerPhone" class="form-label">Customer Phone</label>
+  <input type="text" class="form-control" id="customerPhone" value="${obj.customerPhoneNumber}" required>
+</div>
+<div class="mb-3">
+  <label for="customerEmail" class="form-label">Customer Email</label>
+  <input type="email" class="form-control" id="customerEmail" value="${obj.customerEmailAddress}" required>
+</div>
+<div class="mb-3">
+  <label for="orderType" class="form-label">Order Type</label>
+  <select class="form-select" aria-label="Default select example">
+<option selected>Open this select menu</option>
+<option value="1">One</option>
+<option value="2">Two</option>
+<option value="3">Three</option>
+</select>
+<button type="submit" id="${obj.firebaseKey ? `update-order--${obj.firebaseKey}` : 'submit-order'}" class="btn btn-success">Create/Edit Order</button>
+</div>`;
+};
+
+export default createOrderForm;
