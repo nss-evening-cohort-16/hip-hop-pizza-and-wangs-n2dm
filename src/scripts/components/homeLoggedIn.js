704c5df1835ad2ed3fdc0f2a6d21459370db898e
Messi_G <negash4@gmail.com>
2021-09-16 20:17:43 -0500

    ordercard and itemfrom

--- src/scripts/components/homeLoggedIn.js
+++ src/scripts/components/homeLoggedIn.js
@@ -3,9 +3,9 @@ import clearDom from '../helpers/clearDom';
 const homeLoggedIn = () => {
   clearDom();
   document.querySelector('#button-container').innerHTML = `
-    <button type="button" class="btn btn-primary">View Orders</button>
-    <button type="button" class="btn btn-secondary">Create Order</button>
-    <button type="button" class="btn btn-success"> View Revenue</button>
+    <button type="button" id="view-btn" class="btn btn-primary">View Orders</button>
+    <button type="button" id="create-btn" class="btn btn-secondary">Create Order</button>
+    <button type="button" id="revenue-btn" class="btn btn-success"> View Revenue</button>
     `;
 };
 export default homeLoggedIn;
---
+++ src/scripts/components/orders.js
@@ -0,0 +1,22 @@
+import clearDom from '../helpers/clearDom';
+
+const showOrders = (array) => {
+  clearDom();
+  array.forEach((order) => {
+    document.querySelector('#order-container').innerHTML += `
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+                <p class="card-text">Email: ${order.customeremail}</p>
+                <h5 class="card-title">${order.customername}</h5>
+                <p class="card-text">Phone: ${order.customerphonenumber}</p>
+                <h5 class="card-subtitle mb-2 text-muted">OrderStatus: ${order.isopen}</h5>
+                <p class="card-text">Order Type: ${order.ordertype}</p>
+                <a href="#" class="card-link">Details</a>
+                <a href="#" class="card-link">Edit</a>
+                <a href="#" class="card-link">Delete</a>
+            </div>
+        </div>`;
+  });
+};
+
+export default showOrders;
