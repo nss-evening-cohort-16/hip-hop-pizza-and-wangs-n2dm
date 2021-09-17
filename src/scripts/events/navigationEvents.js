82649d02a2a3b81345b90290ff70ae3f31fdc280
Dario Perez <darioperez1415@gmail.com>
2021-09-17 18:07:04 -0500

    Added files

--- src/scripts/events/navigationEvents.js
+++ src/scripts/events/navigationEvents.js
@@ -1,12 +1,15 @@
 import createOrderForm from '../components/forms/createOrderForm';
-import clearDom from '../helpers/clearDom';
+import showOrders from '../components/orders';
+import getOrders from '../helpers/data/orderData';
 
 const navigationEvents = () => {
   // CREATE ORDER FORM EVENT
   document.querySelector('#createOrder').addEventListener('click', createOrderForm);
 
   // HOMEPAGE
-  document.querySelector('#home').addEventListener('click', clearDom);
+  document.querySelector('#home').addEventListener('click', () => {
+    getOrders().then(showOrders);
+  });
 };
 
 export default navigationEvents;
