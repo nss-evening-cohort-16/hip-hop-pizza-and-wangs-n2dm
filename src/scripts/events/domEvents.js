d91ba2a10bdc2ade9461057c2c74b7eb40b6cd15
Nathan Blackman <nathanpgblackman@gmail.com>
2021-09-18 14:19:33 -0500

    moved delete areas elsewhere

--- src/scripts/events/domEvents.js
+++ src/scripts/events/domEvents.js
@@ -1,24 +1,10 @@
-import createOrderForm from '../components/forms/createOrderForm';
-import { deleteOrders } from '../helpers/data/orderData';
-import showOrders from '../components/orders';
+// import createOrderForm from '../components/forms/createOrderForm';
+// import { deleteOrders } from '../helpers/data/orderData';
+// import showOrders from '../components/orders';
 
 const domEvents = () => {
   document.querySelector('#main-container').addEventListener('click', (e) => {
     // CLICK EVENT FOR CREATING AN ORDER FORM FROM HOME SCREEN
-    if (e.target.id.includes('create-btn')) {
-      console.warn('Clicked the Create Order Button on the Home Page');
-      createOrderForm();
-    }
-    // DELETE ORDER
-    if (e.target.id.includes('delete-order')) {
-      // eslint-disable-next-line no-alert
-      if (window.confirm('Want to delete this order?')) {
-        const [, firebaseKey] = e.target.id.split('--');
-        console.warn('Clicked delete order');
-
-        deleteOrders(firebaseKey).then(showOrders);
-      }
-    }
   });
 };
 
