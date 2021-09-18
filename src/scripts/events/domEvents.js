d30b7c96d3a3e71aeb9a7b1956fd3ec3f2490294
Messi_G <negash4@gmail.com>
2021-09-18 14:16:15 -0500

    code refactoring

--- src/scripts/events/domEvents.js
+++ src/scripts/events/domEvents.js
@@ -1,5 +1,4 @@
 import createOrderForm from '../components/forms/createOrderForm';
-createOrder
 import showOrders from '../components/orders';
 import { getOrders } from '../helpers/data/orderData';
 
@@ -14,27 +13,6 @@ const domEvents = () => {
     if (e.target.id.includes('view-btn')) {
       console.warn('Here is the order card');
       getOrders().then(showOrders);
-
-import { deleteOrders } from '../helpers/data/orderData';
-import showOrders from '../components/orders';
-
-const domEvents = () => {
-  document.querySelector('#main-container').addEventListener('click', (e) => {
-    // CLICK EVENT FOR CREATING AN ORDER FORM FROM HOME SCREEN
-    if (e.target.id.includes('create-btn')) {
-      console.warn('Clicked the Create Order Button on the Home Page');
-      createOrderForm();
-    }
-    // continuing
-    if (e.target.id.includes('delete-order')) {
-      // eslint-disable-next-line no-alert
-      if (window.confirm('Want to delete this order?')) {
-        const [, firebaseKey] = e.target.id.split('--');
-        console.warn('Clicked delete card');
-
-        deleteOrders(firebaseKey).then(showOrders);
-      }
-main
     }
   });
 };
