9c4d20e3352d29177d1f9135ca86d5a9aa6927f9
Dario Perez <darioperez1415@gmail.com>
2021-09-20 09:00:52 -0500

    Added button events to add order

--- src/scripts/views/startApp.js
+++ src/scripts/views/startApp.js
@@ -3,6 +3,7 @@ import navBar from '../components/navBar';
 import domBuilder from '../components/domBuilder';
 import navigationEvents from '../events/navigationEvents';
 import domEvents from '../events/domEvents';
+import buttonEvents from '../events/buttonEvents';
 
 const startApp = () => {
   console.warn('Hip-hop-Pizza!');
@@ -11,5 +12,6 @@ const startApp = () => {
   navigationEvents();
   domEvents();
   homeLoggedIn();
+  buttonEvents();
 };
 export default startApp;
