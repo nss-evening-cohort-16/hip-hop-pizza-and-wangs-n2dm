e624ca789ae88401131b42a1362496c80c074b58
Nissa McKinney <nissa2424@gmail.com>
2021-09-21 19:53:33 -0500

    edit function working

--- src/scripts/components/viewOrder.js
+++ src/scripts/components/viewOrder.js
@@ -1,18 +1,13 @@
-import axios from 'axios';
+/* import axios from 'axios';
 import firebaseConfig from '../../api/apiKeys';
 import { getOrders } from '../helpers/data/orderData';
 
 const dbUrl = firebaseConfig.databaseURL;
 
 const editOrder = (orderObj) => new Promise((resolve, reject) => {
-  axios.post(`${dbUrl}/order.json`, orderObj)
-    .then((response) => {
-      const body = { firebaseKey: response.data.name };
-      axios.patch(`${dbUrl}/orders/${response.data.name}.json`, body)
-        .then(() => {
-          getOrders().then(resolve);
-        });
-    }).catch((error) => reject(error));
+  axios.patch(`${dbUrl}/orders/${orderObj.firebaseKey}.json`, orderObj)
+    .then(() => getOrders().then(resolve))
+    .catch((error) => reject(error));
 });
 
-export default editOrder;
+export default editOrder; */
--- src/scripts/views/startApp.js
+++ src/scripts/views/startApp.js
@@ -4,15 +4,16 @@ import domBuilder from '../components/domBuilder';
 import navigationEvents from '../events/navigationEvents';
 import domEvents from '../events/domEvents';
 import buttonEvents from '../events/buttonEvents';
+import editEvents from '../events/editEvent';
 
 const startApp = () => {
   console.warn('Hip-hop-Pizza!');
   domBuilder();
-  domEvents(); // This adds the Event Listerters to the DOM
   navBar();
   navigationEvents();
   domEvents();
   homeLoggedIn();
   buttonEvents();
+  editEvents();
 };
 export default startApp;
