d90c31967d110b5030db669139c00f819f1883d9
Dario Perez <darioperez1415@gmail.com>
2021-09-22 01:07:48 -0500

    Added items form

--- src/scripts/events/buttonEvents.js
+++ src/scripts/events/buttonEvents.js
@@ -1,6 +1,9 @@
 import createOrderForm from '../components/forms/createOrderForm';
 import showOrders from '../components/orders';
 import { getOrders, createOrder } from '../helpers/data/orderData';
+import itemForm from '../components/forms/itemForm';
+import { createItem } from '../helpers/data/itemData';
+import viewOrderDetail from '../helpers/data/mergedData';
 
 const buttonEvents = () => {
   document.querySelector('#main-container').addEventListener('click', (e) => {
@@ -25,6 +28,22 @@ const buttonEvents = () => {
       };
 
       createOrder(orderObj).then(showOrders);
+    }
+    // DOM EVENT FOR ADDING AN ITEM
+    if (e.target.id.includes('add-item-btn')) {
+      const [, firebaseKey] = e.target.id.split('--');
+      itemForm(firebaseKey);
+    }
+    // Click EVENT FOR SUBMITTING NEW ITEM
+    if (e.target.id.includes('submit-item')) {
+      e.preventDefault();
+      const [, orderId] = e.target.id.split('--');
+      const newItem = {
+        name: document.querySelector('#item-name').value,
+        price: Number(document.querySelector('#item-price').value),
+        orderId
+      };
+      createItem(newItem).then(() => viewOrderDetail(orderId).then(viewOrderDetail));
     }
   });
 };
