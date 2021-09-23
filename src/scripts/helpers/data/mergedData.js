af7104fde241268399a81ae26c95722c8be17079
Nathan Blackman <nathanpgblackman@gmail.com>
2021-09-22 19:49:33 -0500

    working on assembling the functions for the delete Item links

--- src/scripts/helpers/data/mergedData.js
+++ src/scripts/helpers/data/mergedData.js
@@ -1,4 +1,4 @@
-import { getOrderItems } from './itemData';
+import { deleteItem, getOrderItems } from './itemData';
 import { getSingleOrderCard } from './orderData';
 
 // GET ORDER DETAILS
@@ -7,5 +7,16 @@ const viewOrderDetail = async (firebaseKey) => {
   const itemsObject = await getOrderItems(firebaseKey);
   return ({ itemsObject, ...orderObject });
 };
+
+// DELETE ORDER ITEMS
+const deleteOrderItem = (orderId) => new Promise((resolve, reject) => {
+  getOrderItems(orderId).then((orderItemsArray) => {
+    const deleteItems = orderItemsArray.map((item) => deleteItem(item.firebaseKey));
+
+    console.warn(deleteItem);
+
+    Promise.all([...deleteItems]).then(() => resolve(deleteSingleOrder))
+  })
+});
 
 export default viewOrderDetail;
